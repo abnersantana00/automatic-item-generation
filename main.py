0c23c1cb549a9810deacb181e9a8be135357e6f5
Abner de Santana Silva <100228850+abnersantana00@users.noreply.github.com>
2024-10-19 21:56:33 -0300

    second template blank

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from aig_tools import generate_questions
 
-generate_questions('files/condicionais-1-template.json', 'files/condicionais-1-questoes.json')
+# gerar questões 50 questões (if simples)
+#generate_questions('files/condicionais-1-template.json', 'files/condicionais-1-questoes.json')
+# gerar questões 50 questões (if composto)
+generate_questions('files/condicionais-2-template.json', 'files/condicionais-2-questoes.json')
