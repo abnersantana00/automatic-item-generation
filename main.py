10a5f529c231c3f61fa6fbd7644ff317f1440e25
Abner de Santana Silva <100228850+abnersantana00@users.noreply.github.com>
2024-11-08 20:26:45 -0300

    update json

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from aig_tools import generate_questions
 
 # gerar questões 50 questões (if simples)
-generate_questions('files/if-simples-template.json', 'files/if-simples-questoes.json', qtd_itens=10)
-
+#generate_questions('files/if-simples-template.json', 'files/if-simples-questoes.json', qtd_itens=10)
 # gerar questões 50 questões (if composto)
 generate_questions('files/if-composto-template.json', 'files/if-composto-questoes.json', qtd_itens=10)
